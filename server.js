f937cba9da8322cffd129f15e7e94cafc0da3da1
Rahul Kumar <rahulkumar@rigaps.com>
2020-06-23 15:20:18 +0530

    AWS Hosting-1

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at ${process.env.PORT}:${port}`))
